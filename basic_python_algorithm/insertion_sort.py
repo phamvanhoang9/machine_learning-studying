b909a95ccf1c5081647b9bfcbc2ad74cb6106068
Steve Hoang <hoang.pv192875@sis.hust.edu.vn>
2024-01-16 11:02:34 +0700

    __Init__

---
+++ basic_python_algorithm/insertion_sort.py
@@ -0,0 +1,32 @@
+# Ex: array = [2, 8, 4, 1, 3, 7, 6, 5]
+#    sorted_array = [1, 2, 3, 4, 5, 6, 7, 8]
+
+def insertion_sort(array):
+    # We need an outer loop from the second index
+    for i in range(1, len(array)):
+        # an inter loop
+        j = i
+        # compare the current value to the privious value
+        while array[j - 1] > array[j] and j > 0:
+            # swap them
+            array[j - 1], array[j] = array[j], array[j - 1]
+            j -= 1 # decrease the index to further comparing
+    
+    return array
+
+def advanced_insertion(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+# Example:
+array = [2, 8, 4, 1, 7, 6, 5, 3]
+arr = [2, 8, 4, 1, 7, 6, 5, 3]
+print(insertion_sort(array))
+print(advanced_insertion(arr))
