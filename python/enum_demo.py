6e35fecba081f2e11afe2f17c35ec5c5f6c8118e
Steve Hoang <hoang.pv192875@sis.hust.edu.vn>
2024-03-02 10:47:06 +0700

    Enum demo

--- python/enum_demo.py
+++ python/enum_demo.py
@@ -2,6 +2,10 @@
 
 from enum import Enum   
 
+"""
+Enum are used in various senorios in Python where we need to define a set of constants or options
+that we want to present with more expressive and type-safe semantics."""
+
 class Color(Enum):
     RED = 1
     GREEN = 2
