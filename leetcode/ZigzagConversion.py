6151984789abbf675a97dcd883e837b7c10c2dd8
Steve Hoang <hoang.pv192875@sis.hust.edu.vn>
2024-06-01 20:18:55 +0700

    fire

---
+++ leetcode/ZigzagConversion.py
@@ -0,0 +1,28 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        
+        row_arr = [""]*numRows
+        row_idx = 1
+        going_up = True
+
+        for ch in s:
+            row_arr[row_idx - 1] += ch
+            if row_idx == numRows:
+                going_up = False
+            elif row_idx == 1:
+                going_up = True
+
+            if going_up:
+                row_idx += 1
+            else:
+                row_idx -= 1
+            
+        return "".join(row_arr)
+
+if __name__ == "__main__":
+    s = Solution()
+    str = "PAYPALISHIRING"
+    numRows = 4
+    print(s.convert(str, numRows))
