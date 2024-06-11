777d352c76abf7db309b3be6aa3e1d3fa452a0f6
Steve Hoang <hoang.pv192875@sis.hust.edu.vn>
2024-06-11 22:53:34 +0700

    update linked list algorithm

--- dynamic_programming/linked_list.py
+++ dynamic_programming/linked_list.py
@@ -56,6 +56,101 @@ class SinglyLinkedList:
         self.head = new_head 
         self.__count += 1
 
+    def search(self, key):
+        """ 
+        Search for the first node containing data that maches the key
+        Returns the node or `None` if not found
+        Takes O(n) time
+        """
+
+        current = self.head 
+        while current:
+            if current.data == key:
+                return current 
+            else:
+                current = current.next_node
+        
+        return None
+    
+    def insert(self, data, index):
+        """ 
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding node at insertion point takes O(n) time.
+        Takes overall O(n) time.
+        """
+
+        if index >= self.__count:
+            raise IndexError('index out of range')
+        
+        if index == 0:
+            self.add(data) # why self? because add is a method of the class. It means the same as SinglyLinkedList.add(self, data)
+            return # Exit the function after adding the new node
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev_node = current 
+            next_node = current.next_node 
+
+            prev_node.next_node = new 
+            new.next_node = next_node 
+
+        self.__count += 1 
+
+    def node_at_index(self, index):
+        """
+        Returns the Node at specified index
+        Takes O(n) time
+        """
+
+        if index >= self.__count:
+            raise IndexError('index out of range')
+        
+        if index == 0:
+            return self.head
+        
+        current = self.head
+        position = 0
+
+        while position < index:
+            current = current.next_node 
+            position += 1
+
+        return current
+    
+    def remove(self, key):
+        """ 
+        Removes Node containing data that matches the key
+        Returns the node or `None` if key doesn't exist 
+        Takes O(n) time.
+        """
+
+        current = self.head
+        previous = None 
+        found = False 
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True 
+                self.head = current.next_node 
+                self.__count -= 1
+                return current 
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+                self.__count -= 1
+                return current
+            else:
+                previous = current 
+                current = current.next_node
+        
+        return None
+
     def __repr__(self):
         """ 
         Return a string representation of the list.
